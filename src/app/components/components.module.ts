9ab2015b30016de0516c64b829ecaee48e66b82d
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2024-06-25 10:51:28 -0300

    Feat: Add order-summary component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { StepsComponent } from './steps/steps.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 @NgModule({
-  declarations: [HeaderComponent, StepsComponent],
+  declarations: [HeaderComponent, StepsComponent, OrderSummaryComponent],
   imports: [
     CommonModule
   ],
-  exports: [HeaderComponent, StepsComponent]
+  exports: [HeaderComponent, StepsComponent, OrderSummaryComponent]
 })
 export class ComponentsModule { }
