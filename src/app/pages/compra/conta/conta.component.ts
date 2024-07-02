be4ddf5c04afb56bc0650cbefb0f41ddd754c207
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2024-07-02 09:28:08 -0300

    Feat: created a login and register methods

--- src/app/pages/compra/conta/conta.component.ts
+++ src/app/pages/compra/conta/conta.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { PRODUCT_DATA } from '../../../data/productData';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+import { EnvioComponent } from './../envio/envio.component';
 
 
 @Component({
@@ -9,11 +12,41 @@ import { PRODUCT_DATA } from '../../../data/productData';
   styleUrl: './conta.component.scss'
 })
 export class ContaComponent {
-  constructor(
-    private router: Router) { }
-
   step = 1;
   product = PRODUCT_DATA;
+  accountForm: FormGroup = {} as FormGroup;
+  loggedInUser: any;
+  constructor(private fb: FormBuilder, private authService: AuthService,
+    private router: Router) {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.accountForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]]
+    });
+  }
+  onAssign() {
+    if (this.accountForm.valid) {
+      const value = this.accountForm.value;
+      this.authService.login(value.email, value.password);
+    }
+    else {
+      this.accountForm.markAllAsTouched();
+    }
+  }
+  async onRegister() {
+    if (this.accountForm.valid) {
+      const value = this.accountForm.value;
+
+      this.authService.register(value.email, value.password);
+    }
+    else {
+      this.accountForm.markAllAsTouched();
+    }
+  }
+
   goToNextPage() {
     this.router.navigate(['compra/envio']);
   }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private usersKey = 'registeredUsers';
+
+  constructor() {
+    if (!localStorage.getItem(this.usersKey)) {
+      localStorage.setItem(this.usersKey, JSON.stringify([]));
+    }
+  }
+
+  login(email: string, password: string): Observable<boolean> {
+    const users = this.getRegisteredUsers();
+    const user = users.find(u => u.email === email && u.password === password);
+    if (user) {
+      localStorage.setItem('currentUser', JSON.stringify({ email }));
+      return of(true);
+    } else {
+      return of(false);
+    }
+  }
+
+  logout(): void {
+    localStorage.removeItem('currentUser');
+  }
+
+  register(email: string, password: string): Observable<boolean> {
+    const users = this.getRegisteredUsers();
+    const userExists = users.some(u => u.email === email);
+
+    if (userExists) {
+      alert("USER already registered")
+      return of(false);
+    } else {
+      users.push({ email, password });
+
+      localStorage.setItem(this.usersKey, JSON.stringify(users));
+      return of(true);
+    }
+  }
+
+  isLoggedIn(): boolean {
+    const currentUser = localStorage.getItem('currentUser');
+    return !!currentUser;
+  }
+  getUser() {
+    return JSON.parse(localStorage.getItem('currentUser') ?? '');
+  }
+
+
+  private getRegisteredUsers(): { email: string, password: string }[] {
+    const usersString = localStorage.getItem(this.usersKey);
+    return usersString ? JSON.parse(usersString) : [];
+  }
+}
